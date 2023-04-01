e4b4c334930403653c0f7a52c68bdd64ddb6527e
caralinebruz <caraline.bruzinski@gmail.com>
2023-03-31 20:36:28 -0400

    make u and unew be matrices

--- jacobi.cpp
+++ jacobi.cpp
@@ -21,13 +21,34 @@
 
 #include "utils.h"
 
+double** u;
+double** unew;
+
+void free_mems(int N) {
+	// Free the memory after the use of array
+
+ 	for (int i = 0; i < N+2; i++) {
+		delete[] u[i];
+ 	}
+ 	delete[] u;
+
+ 	for (int i = 0; i < N+2; i++) {
+		delete[] unew[i];
+ 	}
+ 	delete[] unew;
+}
+
 // *** UTILS
-void printy(int N, double* q) {
-	// printing f
-	for (int i = 0; i < N; i++) {
-	    std::cout << q[i] << ' ';
+void printy(int N, double** q) {
+
+	for (int i = 0; i < N+2; i++) {
+		for (int j = 0; j < N+2; j++) {
+	    	// cout << A[i][j] << '\t';
+	    	printf("%2lf ", q[i][j]);
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;	
+	
 }
 
 /* compuate global residual, assuming ghost values are updated */
@@ -35,9 +56,15 @@ double compute_residual(double *u, int N, double invhsq)
 {
 	int i;
 	double tmp, res = 0.0;
-#pragma omp parallel for reduction (+:res)
+// #pragma omp parallel for reduction (+:res)
 	for (i = 1; i <= N; i++){
-		tmp = ((2.0*u[i] - u[i-1] - u[i+1]) * invhsq - 1);
+
+		// tmp = ((2.0*u[i] - u[i-1] - u[i+1]) * invhsq - 1);
+
+		// u_new is -2*u[i] + u[i-1] + u[i+1]
+		tmp = ( (2.0*u[i] - u[i-1] - u[i+1] )  * invhsq - 1);
+
+		// std::cout << "temp: " << tmp << std::endl;
 		res += tmp * tmp;
 	}
 	return sqrt(res);
@@ -50,15 +77,33 @@ int main(int argc, char * argv[])
 
 	// sscanf(argv[1], "%d", &N);
 	// sscanf(argv[2], "%d", &max_iters);
-	N = 5;
-	max_iters = 1000;
+	N = 4;
+	max_iters = 100;
 
 	Timer tt;
 	tt.tic();
 
 	/* Allocation of vectors, including left and right ghost points */
-	double* u    = (double *) calloc(sizeof(double), N+2);
-	double* unew = (double *) calloc(sizeof(double), N+2);
+	u = new double*[N+2];
+	unew = new double*[N+2];
+
+	// By using a loop, we will allocate memory to each row of the 2D array.
+	for (int i = 0; i < N+2; i++) {
+		u[i] = new double[N+2];
+		unew[i] = new double[N+2];
+
+		// while we are here, init ends to 0.0
+		u[i][0] = 0.0;
+		u[i][N+1] = 0.0;
+	}
+	// then set top and bottom rows to be 0.0
+	for (int j=0;j<N+2;j++) {
+		u[0][j] = 0.0;
+		u[N+1][j] = 0.0;
+	}
+
+	//double* u    = (double *) calloc(sizeof(double), ((N+2)*(N+2)));
+	//double* unew = (double *) calloc(sizeof(double), ((N+2)*(N+2)));
 
 	double h = 1.0 / (N + 1);
 	double hsq = h*h;
@@ -68,36 +113,45 @@ int main(int argc, char * argv[])
 	printy(N, u);
 
 	/* initial residual */
-	initial_residual = compute_residual(u, N, invhsq);
-	printf("Initial Residual: %g\n",initial_residual);
+	// initial_residual = compute_residual(u, N, invhsq);
+	// printf("Initial Residual: %g\n",initial_residual);
 
-	residual = initial_residual;
-	u[0] = u[N+1] = 0.0;
-	double omega = 1.0; //2./3;
+	// residual = initial_residual;
+
+	// set ghost points to 0.0
+	// u[0] = u[N+1] = 0.0;
 
 
-	// do 1D jacobi until convergence
-	for (iter = 0; iter < max_iters && residual/initial_residual > tol; iter++) {
+	//double omega = 1.0; //2./3;
+	double omega = 1.0;
 
-		/* Jacobi step for all the inner points */
-		for (i = 1; i <= N; i++){
-			unew[i] =  u[i] + omega * 0.5 * (hsq + u[i - 1] + u[i + 1] - 2*u[i]);
-		}
 
-		/* flip pointers; that's faster than memcpy  */
-		// memcpy(u,unew,(N+2)*sizeof(double));
-		double* utemp = u;
-		u = unew;
-		unew = utemp;
-		if (0 == (iter % 1)) {
-			residual = compute_residual(u, N, invhsq);
-			printf("Iter %d: Residual: %g\n", iter, residual);
-		}
-	}
+	// // do 1D jacobi until convergence
+	// for (iter = 0; iter < max_iters && residual/initial_residual > tol; iter++) {
+
+	// 	/* Jacobi step for all the inner points */
+	// 	for (i = 1; i <= N; i++){
+	// 		// unew[i] =  u[i] + omega * 0.5 * (hsq + u[i - 1] + u[i + 1] - 2*u[i]);
+	// 		unew[i] =  u[i] + omega * 0.5 * (hsq + u[i - 1] + u[i + 1] - 2*u[i]);
+	// 	}
+
+	// 	printy(N,unew);
+
+	// 	/* flip pointers; that's faster than memcpy  */
+	// 	// memcpy(u,unew,(N+2)*sizeof(double));
+	// 	double* utemp = u;
+	// 	u = unew;
+	// 	unew = utemp;
+	// 	if (0 == (iter % 1)) {
+	// 		residual = compute_residual(u, N, invhsq);
+	// 		printf("Iter %d: Residual: %g\n", iter, residual);
+	// 	}
+	// }
 
 	/* Clean up */
-	free(u);
-	free(unew);
+	// free(u);
+	// free(unew);
+	free_mems(N);
 
 	/* timing */
 	double t = tt.toc();
